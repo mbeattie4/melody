7be4fb02c557df8b2528382abcaf24730ad93fbd
Matt Beattie <matthewbeattie@pinterest.com>
2021-01-08 22:44:14 -0600

    Update .gitignore and remove unused import

--- app/src/main/java/com/quicksilver/melody/MainActivity.kt
+++ app/src/main/java/com/quicksilver/melody/MainActivity.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.quicksilver.melody.ui.MelodyTheme
-import kotlin.math.absoluteValue
 import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
