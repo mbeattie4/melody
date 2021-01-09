278588a11c86f5b6ce2f29baf377c9bc7960ba83
Matt Beattie <mbeattie313@gmail.com>
2021-01-09 10:26:13 -0600

    Move miscellaneous practice composables to new Miscellaneous file in components directory

--- app/src/main/java/com/quicksilver/melody/MainActivity.kt
+++ app/src/main/java/com/quicksilver/melody/MainActivity.kt
@@ -2,40 +2,18 @@ package com.quicksilver.melody
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Providers
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import com.quicksilver.melody.components.NewsStory
 import com.quicksilver.melody.ui.MelodyTheme
-import kotlin.math.max
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp {
+            MelodyApp {
                 NewsStory()
             }
         }
@@ -43,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-fun MyApp(content: @Composable () -> Unit) {
+fun MelodyApp(content: @Composable () -> Unit) {
     MelodyTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
@@ -52,295 +30,10 @@ fun MyApp(content: @Composable () -> Unit) {
     }
 }
 
-@Composable
-fun NewsStory() {
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        val typography = MaterialTheme.typography
-        val imageModifier = Modifier
-            .preferredHeight(180.dp)
-            .fillMaxWidth()
-            .clip(shape = RoundedCornerShape(16.dp))
-
-        Image(
-            imageResource(id = R.drawable.header),
-            modifier = imageModifier,
-            contentScale = ContentScale.Crop
-        )
-
-        Spacer(modifier = Modifier.preferredHeight(16.dp))
-
-        Text(
-            text = "A day in Shark Fin Cove",
-            style = typography.h6,
-            color = Color.Blue,
-            maxLines = 2,
-            overflow = TextOverflow.Ellipsis,
-            modifier = Modifier.padding(4.dp)
-        )
-        Text(text = "Davenport, California", style = typography.body2)
-        Text(text = "December 2018", style = typography.body2)
-
-        val counterState = remember { mutableStateOf(0) }
-        Counter(
-            count = counterState.value,
-            updateCount = { newCount -> counterState.value = newCount }
-        )
-    }
-}
-
-@Composable
-fun Counter(count: Int, updateCount: (Int) -> Unit) {
-    Button(onClick = { updateCount(count + 1) }) {
-        Text(text = "Clicked $count times")
-    }
-}
-
-@Composable
-fun PhotographerCard(modifier: Modifier = Modifier) {
-    Row(
-        modifier
-            .padding(8.dp)
-            .clip(RoundedCornerShape(4.dp))
-            .background(MaterialTheme.colors.surface)
-            .clickable(onClick = { /* Ignoring onClick */ })
-            .padding(16.dp)
-    ) {
-        Surface(
-            modifier = Modifier.preferredSize(50.dp),
-            shape = CircleShape,
-            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-        ) {
-
-        }
-        Column(
-            modifier = Modifier.padding(start = 8.dp).align(Alignment.CenterVertically)
-        ) {
-            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
-            Providers(AmbientContentAlpha provides ContentAlpha.medium) {
-                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
-            }
-        }
-    }
-}
-
 @Preview
 @Composable
-fun PhotographerCardPreview() {
-    MelodyTheme {
-        PhotographerCard()
-    }
-}
-
-@Composable
-fun LayoutsCodelab() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "LayoutsCodelab")
-                },
-                actions = {
-                    IconButton(onClick = { /* Do something */ }) {
-                        Icon(Icons.Filled.Favorite)
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
-    }
-}
-
-@Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-    ScrollableRow(modifier = modifier) {
-        StaggeredGrid(rows = 6) {
-            for (topic in topics) {
-                Chip(modifier = Modifier.padding(8.dp), text = topic)
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun LayoutsCodelabPreview() {
-    MelodyTheme {
-        LayoutsCodelab()
-    }
-}
-
-@Composable
-fun MyOwnColumn(
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
-) {
-    Layout(
-        modifier = modifier,
-        content = content
-    ) { measurables, constraints ->
-        // Measure children - do not constrain views further (measure with given constraints)
-        val placeables = measurables.map { measurable ->
-            // Measure each child
-            measurable.measure(constraints)
-        }
-
-        // Track the y-coordinate we have placed children up to
-        var yPosition = 0
-
-        // Set the size of the layout to be as big as it can
-        layout(constraints.maxWidth, constraints.maxHeight) {
-            // Place children in the parent layout
-            placeables.forEach { placeable ->
-                // Position item on the screen
-                placeable.placeRelative(x = 0, y = yPosition)
-
-                // Record the y-coordinate placed up to
-                yPosition += placeable.height
-            }
-        }
-    }
-}
-
-@Composable
-fun StaggeredGrid(
-    modifier: Modifier = Modifier,
-    rows: Int = 3,
-    content: @Composable () -> Unit
-) {
-    Layout(
-        modifier = modifier,
-        content = content
-    ) { measureables, constraints ->
-        // Keep track of width of each row
-        val rowWidths = IntArray(rows) { 0 }
-
-        // Keep track of the max height of each row
-        val rowMaxHeights = IntArray(rows) { 0 }
-
-        // Do not constrain child views further, measure them with the given constraints
-        val placeables = measureables.mapIndexed { index, measurable ->
-            // Measure each child
-            val placeable = measurable.measure(constraints)
-
-            // Track the width and max height of each row
-            val row = index % rows
-            rowWidths[row] = rowWidths[row] + placeable.width
-            rowMaxHeights[row] = max(rowMaxHeights[row], placeable.height)
-
-            placeable
-        }
-
-        // Grid's width is widest row coerced to width constraints
-        val width = rowWidths.maxOrNull()
-            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
-
-        // Grid's height is the sum of the tallest element of each row coerced to height constraints
-        val height = rowMaxHeights.sumBy { it }
-            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
-
-        // Y-coordinate of each row - based on height accumulation of previous rows
-        val rowY = IntArray(rows) { 0 }
-        for (i in 1 until rows) {
-            rowY[i] = rowY[i - 1] + rowMaxHeights[i - 1]
-        }
-
-        // Set the size of the parent layout and position children
-        layout(width, height) {
-            // X-coordinate we have placed up to, per row
-            val rowX = IntArray(rows) { 0 }
-
-            placeables.forEachIndexed { index, placeable ->
-                val row = index % rows
-                placeable.placeRelative(
-                    x = rowX[row],
-                    y = rowY[row]
-                )
-                rowX[row] += placeable.width
-            }
-        }
-    }
-}
-
-@Composable
-fun Chip(modifier: Modifier = Modifier, text: String) {
-    Card(
-        modifier = modifier,
-        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-        Row(
-            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                modifier = Modifier.preferredSize(16.dp, 16.dp)
-                    .background(color = MaterialTheme.colors.secondary)
-            )
-            Spacer(Modifier.preferredWidth(4.dp))
-            Text(text = text)
-        }
-    }
-}
-
-@Composable
-fun ConstraintLayoutContent() {
-    ConstraintLayout {
-        val (button1, button2, text) = createRefs()
-
-        Button(
-            onClick = { /* Do something */ },
-            modifier = Modifier.constrainAs(button1) { top.linkTo(parent.top, margin = 16.dp) }
-        ) {
-            Text(text = "Button 1")
-        }
-
-        Text("Text", Modifier.constrainAs(text) {
-            top.linkTo(button1.bottom, margin = 16.dp)
-            centerAround(button1.end)
-        })
-
-        val barrier = createEndBarrier(button1, text)
-        Button(
-            onClick = { /* Do something */ },
-            modifier = Modifier.constrainAs(button2) {
-                top.linkTo(parent.top, margin = 16.dp)
-                start.linkTo(barrier)
-            }
-        ) {
-            Text(text = "Button 2")
-        }
-    }
-}
-
-val topics = listOf(
-    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
-    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
-    "Religion", "Social sciences", "Technology", "TV", "Writing"
-)
-
-@Preview
-@Composable
-fun ChipPreview() {
-    MelodyTheme {
-        Chip(text = "Hi there")
-    }
-}
-
-@Preview
-@Composable
-fun ConstraintLayoutPreview() {
-    MelodyTheme {
-        ConstraintLayoutContent()
-    }
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApp {
+fun MelodyAppPreview() {
+    MelodyApp {
         NewsStory()
     }
 }
